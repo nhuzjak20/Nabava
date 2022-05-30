ee8b726902c248a7eb7eb8036e9fc560b73404db
nhuzjak20 <nhuzjak20@foi.hr>
2022-05-30 16:40:24 +0200

    Odustajemo od logina, napravit ćemo bare bones program po zahtjevima, minimalno ono sta treba

---
+++ Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Nabava
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.nhuzjak20_DBDataSet = new Nabava.nhuzjak20_DBDataSet();
+            this.nhuzjak20DBDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nhuzjak20_DBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nhuzjak20DBDataSetBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.DataSource = this.nhuzjak20DBDataSetBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(33, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(732, 220);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // nhuzjak20_DBDataSet
+            // 
+            this.nhuzjak20_DBDataSet.DataSetName = "nhuzjak20_DBDataSet";
+            this.nhuzjak20_DBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // nhuzjak20DBDataSetBindingSource
+            // 
+            this.nhuzjak20DBDataSetBindingSource.DataSource = this.nhuzjak20_DBDataSet;
+            this.nhuzjak20DBDataSetBindingSource.Position = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Nabava";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nhuzjak20_DBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nhuzjak20DBDataSetBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource nhuzjak20DBDataSetBindingSource;
+        private nhuzjak20_DBDataSet nhuzjak20_DBDataSet;
+    }
+}
