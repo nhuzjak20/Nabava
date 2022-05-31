46d97815ba0d7f81647119a3d8d886c1e7d8adba
nhuzjak20 <nhuzjak20@foi.hr>
2022-05-31 07:30:32 +0200

    Radi, sve

---
+++ Podaci/promjena_zahtjeva.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nabava.Podaci
+{
+    public class promjena_zahtjeva
+    {
+        public int id { get; set; }
+        public int stanje { get; set; }
+    }
+}
