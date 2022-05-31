c4d72fffc5aeaebb338ed78349307c819482ddc0
nhuzjak20 <nhuzjak20@foi.hr>
2022-05-31 02:34:57 +0200

    Radi display, idemo dalje

--- Lista_zahtjeva.Designer.cs
+++ Lista_zahtjeva.Designer.cs
@@ -32,26 +32,18 @@
             this.zahtjeviBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             this.zahtjeviBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.podnesi_zahtjev_gmb = new System.Windows.Forms.Button();
-            this.huzjaknikola_bazaDataSet2 = new Nabava.huzjaknikola_bazaDataSet();
             this.zahtjeviBindingSource2 = new System.Windows.Forms.BindingSource(this.components);
-            this.zahtjeviTableAdapter2 = new Nabava.huzjaknikola_bazaDataSetTableAdapters.zahtjeviTableAdapter();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.podaci_za_faks = new Nabava.Podaci_za_faks();
             this.zahtjeviBindingSource3 = new System.Windows.Forms.BindingSource(this.components);
+            this.podaci_za_faks = new Nabava.Podaci_za_faks();
             this.zahtjeviTableAdapter3 = new Nabava.Podaci_za_faksTableAdapters.zahtjeviTableAdapter();
-            this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.osvjezi_gmb = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.zahtjeviBindingSource1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.zahtjeviBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.huzjaknikola_bazaDataSet2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.zahtjeviBindingSource2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.podaci_za_faks)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.zahtjeviBindingSource3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.podaci_za_faks)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // zahtjeviBindingSource1
@@ -72,93 +64,23 @@
             this.podnesi_zahtjev_gmb.UseVisualStyleBackColor = true;
             this.podnesi_zahtjev_gmb.Click += new System.EventHandler(this.podnesi_zahtjev_gmb_Click);
             // 
-            // huzjaknikola_bazaDataSet2
-            // 
-            this.huzjaknikola_bazaDataSet2.DataSetName = "huzjaknikola_bazaDataSet";
-            this.huzjaknikola_bazaDataSet2.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
             // zahtjeviBindingSource2
             // 
             this.zahtjeviBindingSource2.DataMember = "zahtjevi";
-            this.zahtjeviBindingSource2.DataSource = this.huzjaknikola_bazaDataSet2;
-            // 
-            // zahtjeviTableAdapter2
-            // 
-            this.zahtjeviTableAdapter2.ClearBeforeFill = true;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.AutoGenerateColumns = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.dataGridViewTextBoxColumn6,
-            this.dataGridViewTextBoxColumn7,
-            this.dataGridViewTextBoxColumn8,
-            this.dataGridViewTextBoxColumn9,
-            this.dataGridViewTextBoxColumn10});
-            this.dataGridView1.DataSource = this.zahtjeviBindingSource3;
-            this.dataGridView1.Location = new System.Drawing.Point(31, 21);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(910, 250);
-            this.dataGridView1.TabIndex = 2;
-            // 
-            // podaci_za_faks
-            // 
-            this.podaci_za_faks.DataSetName = "Podaci_za_faks";
-            this.podaci_za_faks.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
             // zahtjeviBindingSource3
             // 
             this.zahtjeviBindingSource3.DataMember = "zahtjevi";
             this.zahtjeviBindingSource3.DataSource = this.podaci_za_faks;
             // 
+            // podaci_za_faks
+            // 
+            this.podaci_za_faks.DataSetName = "Podaci_za_faks";
+            this.podaci_za_faks.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
             // zahtjeviTableAdapter3
             // 
             this.zahtjeviTableAdapter3.ClearBeforeFill = true;
-            // 
-            // dataGridViewTextBoxColumn6
-            // 
-            this.dataGridViewTextBoxColumn6.DataPropertyName = "id";
-            this.dataGridViewTextBoxColumn6.HeaderText = "id";
-            this.dataGridViewTextBoxColumn6.MinimumWidth = 6;
-            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
-            this.dataGridViewTextBoxColumn6.ReadOnly = true;
-            this.dataGridViewTextBoxColumn6.Width = 125;
-            // 
-            // dataGridViewTextBoxColumn7
-            // 
-            this.dataGridViewTextBoxColumn7.DataPropertyName = "naziv_podnositelja";
-            this.dataGridViewTextBoxColumn7.HeaderText = "naziv_podnositelja";
-            this.dataGridViewTextBoxColumn7.MinimumWidth = 6;
-            this.dataGridViewTextBoxColumn7.Name = "dataGridViewTextBoxColumn7";
-            this.dataGridViewTextBoxColumn7.Width = 125;
-            // 
-            // dataGridViewTextBoxColumn8
-            // 
-            this.dataGridViewTextBoxColumn8.DataPropertyName = "nacin_financiranja";
-            this.dataGridViewTextBoxColumn8.HeaderText = "nacin_financiranja";
-            this.dataGridViewTextBoxColumn8.MinimumWidth = 6;
-            this.dataGridViewTextBoxColumn8.Name = "dataGridViewTextBoxColumn8";
-            this.dataGridViewTextBoxColumn8.Width = 125;
-            // 
-            // dataGridViewTextBoxColumn9
-            // 
-            this.dataGridViewTextBoxColumn9.DataPropertyName = "razlog";
-            this.dataGridViewTextBoxColumn9.HeaderText = "razlog";
-            this.dataGridViewTextBoxColumn9.MinimumWidth = 6;
-            this.dataGridViewTextBoxColumn9.Name = "dataGridViewTextBoxColumn9";
-            this.dataGridViewTextBoxColumn9.Width = 125;
-            // 
-            // dataGridViewTextBoxColumn10
-            // 
-            this.dataGridViewTextBoxColumn10.DataPropertyName = "dekan_potvrda";
-            this.dataGridViewTextBoxColumn10.HeaderText = "dekan_potvrda";
-            this.dataGridViewTextBoxColumn10.MinimumWidth = 6;
-            this.dataGridViewTextBoxColumn10.Name = "dataGridViewTextBoxColumn10";
-            this.dataGridViewTextBoxColumn10.Width = 125;
             // 
             // osvjezi_gmb
             // 
@@ -170,24 +92,33 @@
             this.osvjezi_gmb.UseVisualStyleBackColor = true;
             this.osvjezi_gmb.Click += new System.EventHandler(this.osvjezi_gmb_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(45, 17);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(895, 218);
+            this.dataGridView1.TabIndex = 4;
+            // 
             // Lista_zahtjeva
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(970, 317);
-            this.Controls.Add(this.osvjezi_gmb);
+            this.ClientSize = new System.Drawing.Size(970, 340);
             this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.osvjezi_gmb);
             this.Controls.Add(this.podnesi_zahtjev_gmb);
             this.Name = "Lista_zahtjeva";
             this.Text = "Lista_zahtjeva";
             this.Load += new System.EventHandler(this.Lista_zahtjeva_Load);
             ((System.ComponentModel.ISupportInitialize)(this.zahtjeviBindingSource1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.zahtjeviBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.huzjaknikola_bazaDataSet2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.zahtjeviBindingSource2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.podaci_za_faks)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.zahtjeviBindingSource3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.podaci_za_faks)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -213,15 +144,10 @@
         private huzjaknikola_bazaDataSet huzjaknikola_bazaDataSet2;
         private System.Windows.Forms.BindingSource zahtjeviBindingSource2;
         private huzjaknikola_bazaDataSetTableAdapters.zahtjeviTableAdapter zahtjeviTableAdapter2;
-        private System.Windows.Forms.DataGridView dataGridView1;
         private Podaci_za_faks podaci_za_faks;
         private System.Windows.Forms.BindingSource zahtjeviBindingSource3;
         private Podaci_za_faksTableAdapters.zahtjeviTableAdapter zahtjeviTableAdapter3;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn7;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn8;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn9;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn10;
         private System.Windows.Forms.Button osvjezi_gmb;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- Lista_zahtjeva.cs
+++ Lista_zahtjeva.cs
@@ -7,16 +7,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DBLayer;
+using System.Data.SqlClient;
 
 namespace Nabava
 {
     public partial class Lista_zahtjeva : Form
     {
+        //SqlConnection conn = new SqlConnection("Data Source=3sql11.freesqldatabase.com,3306; Initial Catalog=nhuzjak20_DB; User=nhuzjak20; Password=C!79Do+");
+        SqlConnection conn = new SqlConnection(@"Data Source=sql.bsite.net\MSSQL2016; Initial Catalog=huzjaknikola_baza; User=huzjaknikola_baza; Password=nikola55");
+        SqlCommand cmd;
+        DataTable dt;
+        SqlDataAdapter adapter;
         public Lista_zahtjeva()
         {
             InitializeComponent();
+            DisplayValue();
         }
 
+        public void DisplayValue()
+        {
+            conn.Open();
+            adapter = new SqlDataAdapter("SELECT * FROM dbo.zahtjevi", conn);
+            dt = new DataTable();
+            adapter.Fill(dt);   
+            dataGridView1.DataSource = dt; 
+            conn.Close();
+        }
         private void Lista_zahtjeva_Load(object sender, EventArgs e)
         {
             // TODO: This line of code loads data into the 'podaci_za_faks.zahtjevi' table. You can move, or remove it, as needed.
@@ -32,6 +49,22 @@ namespace Nabava
         private void osvjezi_gmb_Click(object sender, EventArgs e)
         {
             this.zahtjeviTableAdapter3.Fill(this.podaci_za_faks.zahtjevi);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void pretrazi_ime_Click(object sender, EventArgs e)
+        {
+            //string pretraga = pretrazi_ime.Text;
+            DB.SetConfiguration(@"sql.bsite.net\MSSQL2016", "huzjaknikola_baza", "nikola55");
+            DB.OpenConnection();
+            //string sequel_upit = "SELECT FROM dbo.zahtjevi WHERE naziv_podnositelja='" + pretraga + "';";
+            //var podaci = new SqlDataAdapter(sequel_upit);
+            var dt = new DataTable();
+           
         }
     }
 }
